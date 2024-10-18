dd4bf0dbfa6b2875bfc2a3c76c1b84b7add0c7d6
AncesLatino2004 <90011449+AncesLatino2004@users.noreply.github.com>
2024-10-18 19:29:16 +0200

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -7,8 +7,7 @@ import java.util.Random;
 public class Enemy {
     private String name;
     private int currentRoom;
-    private Map m = new Map();
-    private Player player = new Player("Hero", m.getRoom(8));
+
     public Enemy(String name, int startRoom) {
         this.name = name;
         this.currentRoom = startRoom;
@@ -22,18 +21,20 @@ public class Enemy {
         return currentRoom;
     }
 
-    public void moveEnemy(String direction, Map map) {
+    // Método para mover al enemigo basado en la dirección
+    public void moveEnemy(int direction, Map map) {
         Room room = map.getRoom(currentRoom);
-        int nextRoomIndex = room.getExit(direction);
+        int nextRoomIndex = room.getExit(direction); // Obtener la salida según la dirección
 
         if (nextRoomIndex != -1) {
-            currentRoom = nextRoomIndex;
+            currentRoom = nextRoomIndex; // Actualizar la habitación actual del enemigo
             System.out.println(name + " se ha movido a: " + map.getRoom(currentRoom).getName());
         } else {
             System.out.println(name + " no puede moverse en esa dirección.");
         }
     }
 
+    // Método de movimiento aleatorio (si es necesario mantenerlo)
     public void moveRandomly(Map map) {
         Room room = map.getRoom(currentRoom);
         List<Integer> validExits = new ArrayList<>();
@@ -52,31 +53,5 @@ public class Enemy {
         } else {
             System.out.println(name + " no puede moverse.");
         }
-    }
-    
-    public void atacar() {
-    	System.out.println();
-    	if(player.hasDonut() == true) {
-    		System.out.println("Gonzalin se ha distraido con los Donuts que llevas encima y no se va a mover mas.");
-    	}
-    	else {
-    		System.out.println("Gonzalin se te hecha encima y te mata");
-    		GameOver();
-    	}
-    	
-    }
-    public void GameOver() {
-    	System.out.println("███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀\n" +
-                "██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼\n" +
-                "██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀\n" +
-                "██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼\n" +
-                "███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄\n" +
-                "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
-                "███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼\n" +
-                "██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼\n" +
-                "██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼\n" +
-                "██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼\n" +
-                "███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄");
-   
     }
 }
