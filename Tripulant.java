4de03df1c2330c055b6a20973fbffb296b6e9809
JS1331 <126185063+JS1331@users.noreply.github.com>
2024-10-27 14:25:51 +0100

    Update Tripulant.java
    Traducció al Català  + features

--- Tripulant.java
+++ Tripulant.java
@@ -1,12 +1,36 @@
 package JocNau;
 
 public class Tripulant extends NPC {
+	private int currentRoom;
+	private Map m = new Map();
+	private boolean despert = false;
+	
 	public Tripulant() {
-        super("Tripulant", "Buenos dias, capitan a que se deve la interrupcion de mi sueño?.");
+        super("Tripulant", "Bon dia, capità. A què es deu la interrupció del meu son?");
     }
 	
 	public void askForCard(){
-		System.out.println("Buenos dias, capitan a que se deve la interrupcion de mi sueño?.");
-		System.out.println("Quiere mi tarjeta de acceso?, aqui tiene capitan");
+		System.out.println("Bon dia, capità. A què es deu la interrupció del meu son?");
+		System.out.println("Vol la meva targeta d'accés? Aquí la té, capità.");
+	}
+	
+	public void moveTripulant(int direction, Map map) {
+	    Room room = map.getRoom(currentRoom);
+	    int nextRoomIndex = room.getExit(direction); // Obtenir la sortida segons la direcció
+
+	    if (nextRoomIndex != -1) {
+	        currentRoom = nextRoomIndex; // Actualitzar l'habitació actual del tripulant
+	        System.out.println("El tripulant s'ha mogut a: " + map.getRoom(currentRoom).getName());
+	    } else {
+	        System.out.println("El tripulant no es pot moure en aquesta direcció.");
+	    }
+	}
+	
+	public void setDespert() {
+		despert = true;
+	}
+	 
+	public boolean getDespert() {
+		return despert;
 	}
 }
